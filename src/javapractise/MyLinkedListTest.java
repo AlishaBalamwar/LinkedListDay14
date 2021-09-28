9759d8c4c87b70bacc29999bba116884d92b1826
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-09-28 14:38:42 +0530

    test and verification of append method

--- src/javapractise/MyLinkedListTest.java
+++ src/javapractise/MyLinkedListTest.java
@@ -13,5 +13,22 @@ public class MyLinkedListTest {
 		myLinkedList1.add(myFirstNode);
 		myLinkedList1.add(mySecondNode);
 		myLinkedList1.add(myThirdNode);
+		boolean result = myThirdNode.getNext().equals(mySecondNode) &&
+				myThirdNode.getNext().getNext().equals(myFirstNode);
+		Assert.assertTrue(result);
+	}
+	
+	@Test
+	public void given3NumbersWhenAddedToTheListShouldBeAppend() {
+		MyNode<Integer> myFirstNode = new MyNode<>(56);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(70);
+		MyLinkedList1 myLinkedList1 = new MyLinkedList1();
+		myLinkedList1.append(myFirstNode);
+		myLinkedList1.append(mySecondNode);
+		myLinkedList1.append(myThirdNode);
+		boolean result = myFirstNode.getNext().equals(mySecondNode) &&
+						myFirstNode.getNext().getNext().equals(myThirdNode);
+		Assert.assertTrue(result);
 	}
 }
