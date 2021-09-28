295dcbb07fdd1715812441403440211860a64b97
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-09-28 14:37:45 +0530

    append method added

--- src/javapractise/MyLinkedList1.java
+++ src/javapractise/MyLinkedList1.java
@@ -20,6 +20,19 @@ public class MyLinkedList1<K> {
 			INode<K> tempNode = this.head;
 			this.head = newNode;
 			this.head.setNext(tempNode);
+			}
+		}
+	
+	public void append(INode<K> newNode) {
+		if(this.tail == null) {
+			this.tail = newNode;
+		}
+			
+		if(this.head == null) {
+			this.head = newNode;
+		}else {
+			this.tail.setNext(newNode);
+			this.tail = newNode;
 		}
 	}
 }
